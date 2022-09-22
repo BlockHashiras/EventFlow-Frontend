da28ae1500d0eb6277a3da5055d28ab50d79a701
kayzee <olaniyikazeem112@gmail.com>
2022-09-22 07:52:53 +0100

    completed index-header

--- layout/header-component/HeaderDescription.tsx
+++ layout/header-component/HeaderDescription.tsx
@@ -5,9 +5,7 @@ import {
     Heading,
     Text,
     Image
-} from "@chakra-ui/react"
-
-
+} from "@chakra-ui/react"; 
 
 
 const HeaderDescription = () => {
@@ -21,9 +19,14 @@ const HeaderDescription = () => {
                 <Button _hover={{"background": "#e7e8e7"}} _active={{"backgroundColor": "#cecece"}} boxShadow="0 0 5px #006040" >Create</Button>
             </Flex>
         </Box>
-        <Box w="50%" m="0 25%" boxSize='3xl'>
+        <Box w="50%" m="0 25%" boxSize='base:3xl md: xl'>
             <Image src='/images/headerImage.png' alt="eventflow-header-image" />
         </Box>
+        <Flex justifyContent="space-evenly" w="50%" m="4% 25%">
+            <Image src="/Metamask-logo.png" alt="metamask-logo" width={200} height="50px" />
+            <Image src="/Primary Lockup Daemon Dark.png" alt="blockdaemon-logo" width={250} height="60px" />
+            <Image src="/logo-red-on-white.png" alt="web3Bridge-logo" width={200} height="60px" />
+        </Flex>
         </>
     );
 };
