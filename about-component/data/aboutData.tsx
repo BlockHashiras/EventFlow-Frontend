05e7db93ac83d77a9a1f4329c1eafd6e19635c47
kayzee <olaniyikazeem112@gmail.com>
2022-10-17 02:58:25 +0100

    added about data

---
+++ about-component/data/aboutData.tsx
@@ -0,0 +1,17 @@
+export const aboutData = [
+    {
+        headerImage: 'mission.png',
+        headers: 'Mission',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        headerImage: "vision.png",
+        headers: 'Vision',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    {
+        headerImage: "drive.png",
+        headers: 'Drive',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+]
